23106f8874d33d02395cd5f77bb5c611d75b990e
sandsq <116196797+sandsq@users.noreply.github.com>
2026-09-09 15:34:19 -0700

    update rgb matrix driver

--- rules.mk
+++ rules.mk
@@ -15,6 +15,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = WS2812
+RGB_MATRIX_DRIVER = ws2812
 
 # LTO_ENABLE = yes
